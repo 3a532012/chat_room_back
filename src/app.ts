fcf0c1a05b61975f1d113fd9b40ce28d30c35457
Chris <kirs29895021@gmail.com>
2023-02-10 15:49:24 +0800

    build connect success

--- src/app.ts
+++ src/app.ts
@@ -29,12 +29,32 @@ import ChatRoomSocket from './socket/ChatRoomSocket';
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app)
-const io = new ChatRoomSocket(server, { path: '/chat_room' });
+const io = new ChatRoomSocket(server, { path: '/chat_room', transports :['polling', 'websocket'], cors: {
+  origin: ['http://127.0.0.1:5173']
+} });
 
 
-// ChatRoomSocket(server,'/chat_room')
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 // load router
 for (const route of router) {
--- src/socket/SocketAbstract.ts
+++ src/socket/SocketAbstract.ts
@@ -5,8 +5,8 @@ abstract class Sockets {
     public io:Server;
     constructor(server:http.Server, options?:Partial<ServerOptions>) {
         this.io = new Server(server, options)
-        this.io.on("connect", this.connect)
-        this.io.on("disconnect", this.disconnect)
+        this.io.on("connection", this.connect)
+        this.io.on("disconnection", this.disconnect)
     }
     protected abstract connect(socket:Socket) :void;
     protected abstract disconnect(socket:Socket) :void;
